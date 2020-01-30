e0a4991ca6dc8f1a2a6f4982550d4945b3bc0bf3
Chun Tao Lin <chuntao.lin@mymail.champlain.edu>
2020-01-29 22:37:32 -0500

    Made progress on lambert color lighting, seem to be getting a couple done.

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/02-shading/drawLambert_multi_fs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/02-shading/drawLambert_multi_fs4x.glsl
@@ -65,21 +65,40 @@ float CalculateLambertianProduct(vec4 surfaceNormal, vec4 lightNormal)
 
 void main()
 {
-	float lambertianProduct = CalculateLambertianProduct(mVNormal, CalculateLightVector(viewPosition,uLightPos[0]));
-//	lambertianProduct += normalize(CalculateLambertianProduct(mVNormal, CalculateLightVector(viewPosition,uLightPos[1])));
-//	lambertianProduct += normalize(CalculateLambertianProduct(mVNormal, CalculateLightVector(viewPosition,uLightPos[2])));
-//	lambertianProduct += normalize(CalculateLambertianProduct(mVNormal, CalculateLightVector(viewPosition,uLightPos[3])));
-//
+	float lambertianProduct = CalculateLambertianProduct(mVNormal, CalculateLightVector(viewPosition,uLightPos[0]));//white spot
+	float lambertianProduct2 = CalculateLambertianProduct(mVNormal, CalculateLightVector(viewPosition,uLightPos[1]));//purplewhite
+	float lambertianProduct3 = CalculateLambertianProduct(mVNormal, CalculateLightVector(viewPosition,uLightPos[2]));//yellowspot
+	float lambertianProduct4 = CalculateLambertianProduct(mVNormal, CalculateLightVector(viewPosition,uLightPos[3]));//bluespot
+
+	float lambertianProductSum = lambertianProduct + lambertianProduct2 + lambertianProduct3 + lambertianProduct4;
+//	lambertianProduct += CalculateLambertianProduct(mVNormal, CalculateLightVector(viewPosition,uLightPos[1]));
+	//lambertianProduct += CalculateLambertianProduct(mVNormal, CalculateLightVector(viewPosition,uLightPos[2]));
+	//lambertianProduct += CalculateLambertianProduct(mVNormal, CalculateLightVector(viewPosition,uLightPos[3]));
+
 	vec4 originalTex = texture(uTex_dm, aTexCoord.xy); 
-//	originalTex = mix(originalTex,uLightCol[0],.1);
-//	originalTex = mix(originalTex,uLightCol[1],.1);
-//	originalTex = mix(originalTex,uLightCol[2],.1);
-//	originalTex = mix(originalTex,uLightCol[3],.1);
+
+	vec4 color1 = mix(originalTex,uLightCol[0],0.1) * lambertianProduct;
+	vec4 color2 = mix(originalTex,uLightCol[1],0.2) * lambertianProduct2;
+	vec4 color3 = mix(originalTex,uLightCol[2],0.2) * lambertianProduct3;
+	vec4 color4 = mix(originalTex,uLightCol[3],0.6) * lambertianProduct4;
+	
 	//originalTex = originalTex * CalculateSpecularCoefficient(uLightPos[0],viewPosition,lambertianProduct);
-
+	//vec4 mixedColors = color1;//mix(color1, color3,.3);
+	//mixedColors = mix(mixedColors, color2,.3);
+	//mixedColors = mix(mixedColors, color4,.5);
 	//result = mix(originalTex,uLightCol[0],.5);
-	//vec4 result = mix(uLightCol[0],originalTex,1);
+//	//vec4 result = mix(uLightCol[0],originalTex,1);
+//
+//	vec4 color1 = uLightCol[0] * lambertianProduct;
+//	vec4 color2 = uLightCol[1] * lambertianProduct2; 
+//	vec4 color3 = uLightCol[2] * lambertianProduct3;
+//	vec4 color4 = uLightCol[3] * lambertianProduct4;
+//
+//	color1 = mix(originalTex,color1,1.0);
+	vec4 mixedColors = mix(originalTex, color1,.2);
+	mixedColors = mix(mixedColors, color2,.4);
+	mixedColors = mix(mixedColors, color3,.2);
+	mixedColors = mix(mixedColors, color4,.3);
 
-
-	rtFragColor = originalTex * lambertianProduct;//originalTex;//color1;//mix(color1,lambertianProduct,.5);//lambertianProduct;
+	rtFragColor = mixedColors * lambertianProductSum;//mixedColors * lambertianProductSum;
 }
