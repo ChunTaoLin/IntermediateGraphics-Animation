5ae3e33927909fe0efd69cc54684b42be8524f45
Chun Tao Lin <chuntao.lin@mymail.champlain.edu>
2020-02-11 13:35:37 -0500

    Made progress with the sobel algorithm.

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/04-multipass/drawTexture_outline_fs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/04-multipass/drawTexture_outline_fs4x.glsl
@@ -27,11 +27,69 @@
 // ****TO-DO: 
 //	0) copy existing texturing shader
 //	1) implement outline algorithm - see render code for uniform hints
+uniform sampler2D uTex_dm;
+uniform sampler2D uImage1;
 
+layout (location = 8) in vec4 aTexCoord;
+
+mat3 sobelX = mat3(
+1.0,2.0,1.0,
+0.0,0.0,0.0,
+-1.0,-2.0,-1.0
+
+);
+
+mat3 sobelY = mat3(
+1.0,0.0,-1.0,
+2.0,0.0,-2.0,
+1.0,0.0,-1.0
+
+);
+
+vec3 diff;
+vec3 diff2;
 out vec4 rtFragColor;
 
 void main()
 {
-	// DUMMY OUTPUT: all fragments are OPAQUE DARK GREY
-	rtFragColor = vec4(0.2, 0.2, 0.2, 1.0);
+	diff = texture(uTex_dm,aTexCoord.xy).rgb;
+	mat3 I;
+	
+	//place gradient matrix over each pixel of the image to determine the difference.
+	for (int i  = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3; j++)
+		{
+			vec3 Sample = texelFetch(uTex_dm,ivec2(gl_FragCoord)+ ivec2(i -1,j-1),0).rgb;
+			I[i][j] = length(Sample);
+		}
+	}
+
+	float gradientX = dot(sobelX[0],I[0]) + dot(sobelX[1],I[1]) + dot(sobelX[2],I[2]);
+	float gradientY = dot(sobelY[0],I[0]) + dot(sobelY[1],I[1]) + dot(sobelY[2],I[2]);
+
+	float gradient = sqrt(pow(gradientX,2.0)+pow(gradientY,2.0));
+
+	gradient = smoothstep(0.4,0.6,gradient);
+
+	// Apply texture onto given pixel
+	rtFragColor = vec4(diff - gradient,1.0);
+
+	diff2 = texture(uImage1,aTexCoord.xy).rgb;
+
+	//place gradient matrix over each pixel of the image to determine the difference.
+	for (int i  = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3; j++)
+		{
+			vec3 Sample = texelFetch(uImage1,ivec2(gl_FragCoord)+ ivec2(i -1,j-1),0).rgb;
+			I[i][j] = length(Sample);
+		}
+	}
+
+	float gradientX2 = dot(sobelX[0],I[0]) + dot(sobelX[1],I[1]) + dot(sobelX[2],I[2]);
+	float gradientY2 = dot(sobelY[0],I[0]) + dot(sobelY[1],I[1]) + dot(sobelY[2],I[2]);
+
+	float gradient2 = sqrt(pow(gradientX2,2.0)+pow(gradientY2,2.0));
+		rtFragColor = rtFragColor + vec4(diff2 - gradient2,1.0);
 }
