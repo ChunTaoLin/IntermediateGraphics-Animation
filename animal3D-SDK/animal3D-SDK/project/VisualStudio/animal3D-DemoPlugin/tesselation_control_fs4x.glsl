c05df31a9c2e935c46c1a498cc95ecceb6f7ede4
Chun Tao Lin <chuntao.lin@mymail.champlain.edu>
2020-03-25 20:05:57 -0400

    Trying to figure out the vertices error

--- animal3D-SDK/animal3D-SDK/project/VisualStudio/animal3D-DemoPlugin/tesselation_control_fs4x.glsl
+++ animal3D-SDK/animal3D-SDK/project/VisualStudio/animal3D-DemoPlugin/tesselation_control_fs4x.glsl
@@ -1,26 +1,43 @@
-
-//the out varying float value that will be passed to the fragment shader to determine color
-in float noiseValue;
-in vec4 tessPos;
-
-struct
-{
-    vec4 gl_Position;
-    float gl_PointSize;
-    float gl_ClipDistance[ 6 ];
-} gl_in[ ];
+﻿#version 410 core
+layout(vertices = 3) out;
+const int i = 5;
+//in int gl_PatchVerticesIn;
 
 
+
+////the out varying float value that will be passed to the fragment shader to determine color
+//in float noiseValue;
+//in vec4 tessPos;
+//
+//struct
+//{
+//
+//    vec4 gl_Position;
+//    float gl_PointSize;
+//    float gl_ClipDistance[ 6 ];
+//} gl_in[];
+//
+
+//in gl_PerVertex
+//{
+//  vec4 gl_Position;
+//  float gl_PointSize;
+//  float gl_ClipDistance[];
+//} gl_in[gl_MaxPatchVertices];
 //struct
 //{
 //vec4 gl_Position;
 //float gl_PointSize;
 //float gl_ClipDistance[ 6 ];
 //} gl_out[ ];
-//
-uniform mat4 uMVP;	// (1)
 
-void main()
+//uniform mat4 uMVP;	// (1)
+
+
+void main( )
 {
-    tessPos.a -= 0.5;
+  gl_out[ gl_InvocationID ].gl_Position = gl_in[ gl_InvocationID ].gl_Position;
+
+  gl_TessLevelOuter[0] = float( 1 );
+  gl_TessLevelOuter[1] = float( 5 );
 }
--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/ts/tesselation_evaluation_fs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/ts/tesselation_evaluation_fs4x.glsl
@@ -1,20 +1,22 @@
+#version 410 core
 
-struct
-{
-    vec4 gl_Position;
-    float gl_PointSize;
-    float gl_ClipDistance[ 6 ];
-} gl_in[ ];
+layout(vertices = 3) out;
+//struct
+//{
+//    vec4 gl_Position;
+//    float gl_PointSize;
+//    float gl_ClipDistance[ 6 ];
+//} gl_in[ ];
 
-uniform mat4 uMVP;	// (1)
-
+//uniform mat4 uMVP;	// (1)
+//
 void main()
 {
 	// DUMMY OUTPUT: directly assign input position to output position
 //	gl_Position = aPosition;
-	vec4 newPos = vec4(0,0,0,1);
+//	vec4 newPos = vec4(0,0,0,1);
    // noiseValue = calculatePerlinNoise(newPos.xyz);
 //    newPos.a += calculatePerlinNoise(newPos.xyz);
    // tessPos = newPos;
-	gl_Position = uMVP * newPos;	// (2)
+	//gl_Position = uMVP * newPos;	// (2)
 }
